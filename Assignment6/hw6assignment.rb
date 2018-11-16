d2cf27a16add095e1ea3b76ffc69778d0a022b71
Noah Boulton <noah.boulton@gmail.com>
2018-11-16 10:31:03 -0800

    Added u key functionality

--- Assignment6/hw6assignment.rb
+++ Assignment6/hw6assignment.rb
@@ -7,25 +7,68 @@
 class MyPiece < Piece
     # The constant All_My_Pieces should be declared here:
     # class array holding all the pieces and their rotations
-    All_Pieces = [[[[0, 0], [1, 0], [0, 1], [1, 1]]],  # square (only needs one)
-    rotations([[0, 0], [-1, 0], [1, 0], [0, -1]]), # T
-    [[[0, 0], [-1, 0], [1, 0], [2, 0]], # long (only needs two)
-    [[0, 0], [0, -1], [0, 1], [0, 2]]],
-    rotations([[0, 0], [0, -1], [0, 1], [1, 1]]), # L
-    rotations([[0, 0], [0, -1], [0, 1], [-1, 1]]), # inverted L
-    rotations([[0, 0], [-1, 0], [0, -1], [1, -1]]), # S
-    rotations([[0, 0], [1, 0], [0, -1], [-1, -1]])] # Z
+
+    All_My_Pieces = [[[[0, 0], [1, 0], [0, 1], [1, 1]]],  # square (only needs one)
+               rotations([[0, 0], [-1, 0], [1, 0], [0, -1]]), # T
+               [[[0, 0], [-1, 0], [1, 0], [2, 0]], # long (only needs two)
+               [[0, 0], [0, -1], [0, 1], [0, 2]]],
+               [[[0, 0], [-1, 0], [1, 0], [-2, 0], [2, 0]], # extra long (needs two)
+               [[0, 0], [0, -1], [0, 1], [0, -2], [0, 2]]],
+               [[0, 0]],
+               rotations([[0, 0], [0, -1], [0, 1], [1, 1]]), # L
+               rotations([[0, 0], [0, -1], [0, 1], [-1, 1]]), # inverted L
+               rotations([[0, 0], [-1, 0], [0, -1], [1, -1]]), # S
+               rotations([[0, 0], [1, 0], [0, -1], [-1, -1]])] # Z
     # Your Enhancements here
+    def self.next_piece (board)
+        MyPiece.new(All_My_Pieces.sample, board)
+    end
+
+    
 end
 
 class MyBoard < Board
     # Your Enhancements here:
+    # gets the next piece
+    def next_piece
+        @current_block = MyPiece.next_piece(self)
+        @current_pos = nil
+    end
+
+    # gets the information from the current piece about where it is and uses this
+  # to store the piece on the board itself.  Then calls remove_filled.
+  def store_current
+    locations = @current_block.current_rotation
+    displacement = @current_block.position
+    (0..(locations.length - 1)).each{|index| 
+      current = locations[index];
+      @grid[current[1]+displacement[1]][current[0]+displacement[0]] = 
+      @current_pos[index]
+    }
+    remove_filled
+    @delay = [@delay - 2, 80].max
+  end
 
 end
 
 class MyTetris < Tetris
     # Your Enhancements here
-    # @root.bind('u' , proc {@board.rotate_counter_clockwise; @board.rotate_counter_clockwise})
+    def initialize
+        super()
+    end
+
+    def set_board
+        @canvas = TetrisCanvas.new
+        @board = MyBoard.new(self)
+        @canvas.place(@board.block_size * @board.num_rows + 3,
+                      @board.block_size * @board.num_columns + 6, 24, 80)
+        @board.draw
+    end
+
+    def key_bindings  
+        super()
+        @root.bind('u' , proc {@board.rotate_counter_clockwise; @board.rotate_counter_clockwise})
+    end
 
 end
 
