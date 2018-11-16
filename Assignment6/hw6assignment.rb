dad3a7d0aa66842df94e92e31a9bc9ff0b35ee03
Noah Boulton <noah.boulton@gmail.com>
2018-11-16 13:32:54 -0800

    Added new pieces and cheating functionality

--- Assignment6/hw6assignment.rb
+++ Assignment6/hw6assignment.rb
@@ -9,30 +9,56 @@ class MyPiece < Piece
     # class array holding all the pieces and their rotations
 
     All_My_Pieces = [[[[0, 0], [1, 0], [0, 1], [1, 1]]],  # square (only needs one)
-               rotations([[0, 0], [-1, 0], [1, 0], [0, -1]]), # T
-               [[[0, 0], [-1, 0], [1, 0], [2, 0]], # long (only needs two)
-               [[0, 0], [0, -1], [0, 1], [0, 2]]],
-               [[[0, 0], [-1, 0], [1, 0], [-2, 0], [2, 0]], # extra long (needs two)
-               [[0, 0], [0, -1], [0, 1], [0, -2], [0, 2]]],
-               [[0, 0]],
-               rotations([[0, 0], [0, -1], [0, 1], [1, 1]]), # L
-               rotations([[0, 0], [0, -1], [0, 1], [-1, 1]]), # inverted L
-               rotations([[0, 0], [-1, 0], [0, -1], [1, -1]]), # S
-               rotations([[0, 0], [1, 0], [0, -1], [-1, -1]])] # Z
+                    rotations([[0, 0], [-1, 0], [1, 0], [0, -1]]), # T
+                    [[[0, 0], [-1, 0], [1, 0], [2, 0]], # long (only needs two)
+                    [[0, 0], [0, -1], [0, 1], [0, 2]]],
+                    [[[0, 0], [-1, 0], [1, 0], [-2, 0], [2, 0]], # extra long (needs two)
+                    [[0, 0], [0, -1], [0, 1], [0, -2], [0, 2]]],
+                    rotations([[-1, 0], [0,0], [-1, -1]]), # extra small L 
+                    rotations([[0, 0], [1, 0], [0, 1], [1, 1], [2, 1]]), # extra odd piece 
+                    rotations([[0, 0], [0, -1], [0, 1], [1, 1]]), # L
+                    rotations([[0, 0], [0, -1], [0, 1], [-1, 1]]), # inverted L
+                    rotations([[0, 0], [-1, 0], [0, -1], [1, -1]]), # S
+                    rotations([[0, 0], [1, 0], [0, -1], [-1, -1]])] # Z
+    
+                
     # Your Enhancements here
     def self.next_piece (board)
-        MyPiece.new(All_My_Pieces.sample, board)
+        if @cheat
+            @cheat = false
+            board.lower_score
+            MyPiece.new([[[0,0]]], board)
+        else
+            MyPiece.new(All_My_Pieces.sample, board)
+        end
     end
 
+    def self.cheat ()
+        @cheat = true
+    end
     
 end
 
 class MyBoard < Board
     # Your Enhancements here:
+
+    def initialize (game)
+        @grid = Array.new(num_rows) {Array.new(num_columns)}
+        @current_block = MyPiece.next_piece(self)
+        @score = 0
+        @game = game
+        @delay = 500
+    end
+
     # gets the next piece
     def next_piece
         @current_block = MyPiece.next_piece(self)
         @current_pos = nil
+    end
+
+    def lower_score()
+        @score = @score - 100
+        @game.update_score
     end
 
     # gets the information from the current piece about where it is and uses this
@@ -68,6 +94,10 @@ class MyTetris < Tetris
     def key_bindings  
         super()
         @root.bind('u' , proc {@board.rotate_counter_clockwise; @board.rotate_counter_clockwise})
+        @root.bind('c' , proc   {if @board.score > 99
+                                    MyPiece.cheat()
+                                end
+                                })
     end
 
 end
